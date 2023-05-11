80a5a79d71e72239b39a669b82db5f288aac64d5
nahuelshim <127103454+nahuelshim@users.noreply.github.com>
2023-05-11 09:49:54 +0200

    first commit

---
+++ N1Ex1/S04T01N01ShimMartinezNahuel/src/main/java/cat/itacademy/barcelonactiva/shimmartinez/nahuel/s04/t01/n01/HelloWordController.java
@@ -0,0 +1,23 @@
+package cat.itacademy.barcelonactiva.shimmartinez.nahuel.s04.t01.n01;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWordController {
+
+    @GetMapping("/HelloWorld")
+    public String saluda (@RequestParam (defaultValue = "UNKNOWN") String nom) {
+        return "Hello, " + nom + " you are running a Maven project.";
+    }
+
+   @GetMapping(value = {"/HelloWorld2","/HelloWorld2/{nom}"})
+   public String Saluda2(@PathVariable(required = false)String nom){
+       if(nom == null) {
+           nom = "UNKNOWN";
+       }
+       return "Hello, " + nom + " you are running a Maven project";
+   }
+}
---
+++ N2Ex1/S04T01N02ShimMartinezNahuel/src/test/java/cat/itacademy/barcelonactiva/shimmartinez/nahuel/s04/t01/n02/S04T01N02ShimMartinezNahuelApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.shimmartinez.nahuel.s04.t01.n02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S04T01N02ShimMartinezNahuelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
